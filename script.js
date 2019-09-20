111ba238887a795cf41efb548cdee6aee2151aba
Ubuntu <ubuntu@ip-172-31-23-235.ap-southeast-1.compute.internal>
2019-09-20 03:40:07 +0000

    Able to display list of employee names

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
 /* global $ */
+
 $(function() {
+    
+    function initPageOne()
+    {
+        $('#employee-list li').remove();
+        for (let each_employee of employees)
+        {
+            let each_name = each_employee.name;
+            let each_employee_number = each_employee.employee_number;
+            $("#employee-list")
+            .append(`<li class='employee-link' data-employee-number='${each_employee_number}'>${each_name}`);
+       
+        }
+    }
+    
+    // employee is our 'database'
+    let employees = [
+        {
+            'name': 'John',
+            'employee_number': 'E1234567',
+
+            'duration': 200,
+            'annual_leave': 14,
+            'salary': 3200
+        }, 
+        {
+            'name': 'Alan',
+            'employee_number': 'E2121212',
+            'duration': 720,
+            'annual_leave': 14,
+            'salary': 4500
+        },
+        {
+            'name': 'Cindy',
+            'employee_number': 'E11223344',
+            'duration': 720,
+            'annual_leave': 14,
+            'salary': 4500
+        }];
+    
     // when the webpage first loaded, we hide
     // all the pages but the first page
     $('.page').hide();
     $('#page-one').show();
-
+    
+    initPageOne();
 
     $(".nav-link").click(function(){
         // extract out the value of data-dest attribute for the
@@ -13,6 +54,14 @@ $(function() {
         $('.page').hide();
         $('#' + page).show();
     })
+
+    $('#page-one-link').click(function(){
+        // for each employee, extract out
+        // the link and display it in the
+        // employee-list <ul>
+      
+       initPageOne();
+    });
 
     // VERSION 1 CODE - the easiest and most straightforward method
     // but is not scalable:
@@ -31,4 +80,12 @@ $(function() {
     //     $('.page').hide();
     //     $('#page-three').show();
     // })
+    
+    // EVENT DELGATION
+    $('#employee-list').on('click', '.employee-link', function(){
+       let employee_number= $(this).data('employee-number');
+       
+       // linear search
+    
+    });
 })
