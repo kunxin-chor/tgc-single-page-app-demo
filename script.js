d51d47a36475fe36eb8995fcee8925d6ff3603c7
Ubuntu <ubuntu@ip-172-31-23-235.ap-southeast-1.compute.internal>
2019-09-20 02:16:51 +0000

    Implemented switching page one to page two

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ $(function(){
     // all the pages but the first page
     $('.page').hide();
     $('#page-one').show();
+    
+    $('#page-two-link').click(function(){
+        $('.page').hide();
+        $('#page-two').show();
+    })
 })
