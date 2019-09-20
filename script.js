c4c1e54c8d715917765e07cc1a69ad9683c8b2f8
Ubuntu <ubuntu@ip-172-31-23-235.ap-southeast-1.compute.internal>
2019-09-20 02:22:02 +0000

    Added in page switching between all the pages

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 /* global $ */
-$(function(){
+$(function() {
     // when the webpage first loaded, we hide
     // all the pages but the first page
     $('.page').hide();
     $('#page-one').show();
-    
-    $('#page-two-link').click(function(){
+
+
+    $('#page-one-link').click(function() {
+        $('.page').hide(); // hide all the other pages
+        $('#page-two').show(); // show the page that the user wants to see
+    })
+
+    $('#page-two-link').click(function() {
         $('.page').hide();
         $('#page-two').show();
+    })
+
+    $('#page-three-link').click(function() {
+        $('.page').hide();
+        $('#page-three').show();
     })
 })
